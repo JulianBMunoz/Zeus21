1f86685c9d253163a444b33490bb038a4a6e79a3
slibanore <sarah.libanore@gmail.com>
2026-08-27 14:59:17 +0300

    TEST 2

--- zeus21/maps.py
+++ zeus21/maps.py
@@ -13,8 +13,6 @@ arXiv:2302.08506, arXiv:2306.09403, arXiv:2407.18294, Sklansky et al. (in prep)
 """
 
 
-print('MAAAAAAAAAAAAAAAAPS')
-
 from . import cosmology
 from . import z21_utilities
 from . import inputs
