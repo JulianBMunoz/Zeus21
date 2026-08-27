14fc9da8f84373f12bb010e9401d8a107217fdc5
slibanore <sarah.libanore@gmail.com>
2026-08-27 11:26:49 +0300

    fixed T21maps so it can run more z

--- zeus21/maps.py
+++ zeus21/maps.py
@@ -662,7 +662,7 @@ class T21_maps:
         ### get power spectra
         self._Dsq_T21_lin = ((PowerSpectra.Deltasq_T21_lin[_iz].T / CoeffStructure.T21avg[_iz]**2) * self.T21avg**2).T
         self._Dsq_T21 = ((PowerSpectra.Deltasq_T21[_iz].T / CoeffStructure.T21avg[_iz]**2) * self.T21avg**2).T
-        self._PdT21 = (PowerSpectra.Deltasq_dT21[_iz]/CoeffStructure.T21avg[_iz])* self.T21avg/self._k3over2pi2
+        self._PdT21 = ((PowerSpectra.Deltasq_dT21[_iz].T / CoeffStructure.T21avg[_iz]) * self.T21avg).T / self._k3over2pi2
         self._Pd = (PowerSpectra.Deltasq_d_lin[_iz,:])/self._k3over2pi2
 
         ### generate densities
